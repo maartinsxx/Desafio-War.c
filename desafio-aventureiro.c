b2840bb82afa4afb25aaa1de74b9c9d89b8fc392
Ster Macharet <143458633+maartinsxx@users.noreply.github.com>
2025-12-06 11:28:49 -0300

    Atualizar o desafio-aventureiro.c

--- desafio-aventureiro.c
+++ desafio-aventureiro.c
@@ -7,7 +7,18 @@
 #define MAX_COR 10
 
 typedef struct {
-char nome[MAX_NOME];
-char cor[MAX_COR];
-int tropas;
+   char nome[MAX_NOME];
+   char cor[MAX_COR];
+   int tropas;
 } Território;
+
+/* Função: LimparBuffer
+   Limpa o restante do buffer da entrada (stdin) para evitar problemas com fgets/scanf.)
+
+void LimparBuffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+    }
+    
+/* Função: cadastroTerritorios 
+   Aloca dinamicamente (Calloc) e preenche o vetor de territórios.
